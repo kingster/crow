275db28894b4dc96f2218c31fcf2a1b96add48be
Morten Kristensen <me@mortens.dev>
2019-05-09 20:23:22 +0200

    [cmake] Compiler optimization, warning, and error flags

--- cmake/compilation.cmake
+++ cmake/compilation.cmake
@@ -2,6 +2,40 @@
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
+# Additoinal warnings.
+set(COMMON_COMPILER_WARNINGS "-Wno-unused-parameter -Wempty-body -Woverloaded-virtual -Wtautological-compare")
+set(CLANG_WARNINGS "-Wnull-arithmetic -Woverriding-method-mismatch")
+set(GCC_WARNINGS "-Wuseless-cast")
+
+# Warnings turned into errors.
+set(COMMON_COMPILER_ERRORS "-Werror=return-type -Werror=delete-incomplete -Werror=delete-non-virtual-dtor -Werror=float-equal -Werror=reorder -Werror=uninitialized -Werror=unreachable-code")
+set(CLANG_ERRORS "-Werror=inconsistent-missing-override -Werror=unused-private-field -Werror=division-by-zero -Werror=return-stack-address")
+set(GCC_ERRORS "")
+
 if (NOT MSVC)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -pedantic -Wextra")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic-errors")
+
+  # Clang/GCC
+  set(REL_OPTS "-pipe -fvisibility=hidden -fvisibility-inlines-hidden -ffast-math -funroll-loops")
+
+  # GCC only
+  if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+    set(REL_OPTS "${REL_OPTS} -fno-implement-inlines")
+
+  # Clang only
+  elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+    set(REL_OPTS "${REL_OPTS} -Ofast")
+  endif()
+
+  set(CMAKE_CXX_FLAGS_MINSIZEREL "${CMAKE_CXX_FLAGS_MINSIZEREL} ${REL_OPTS}")
+  set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} ${REL_OPTS}")
+  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} ${REL_OPTS}")
+endif()
+
+message("Compiler: ${CMAKE_CXX_COMPILER_ID} (${CMAKE_CXX_COMPILER_VERSION})")
+
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${GCC_WARNINGS} ${GCC_ERRORS}")
+elseif ("${CMAKE_CXX_COMPILER_ID}" MATCHES "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CLANG_WARNINGS} ${CLANG_ERRORS}")
 endif()
